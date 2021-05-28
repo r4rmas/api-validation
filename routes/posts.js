c8a0155712a0135357f4ab1a8198cce0a880168e
Raul Armas <ra@raularmas.com>
2021-05-27 23:21:19 -0500

    ...

--- routes/posts.js
+++ routes/posts.js
@@ -4,7 +4,8 @@ const verify = require("./verifyToken");
 
 router.get("/", verify, async (req, res) => {
   const user = await User.findById(req.user);
-  //ya que el jwt lo unico que tiene es el _id no es necesario especificarlo (el iat es default)
+  //ya que el jwt lo unico que tiene es el _id no es necesario
+  //especificarlo (el iat es default)
   res.status(200).send(user);
 });
 
